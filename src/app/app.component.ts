1c04fcdfefefbe01ea1d7e00254f798272dbaab7
Sui <peng.sui@accenture.com>
2021-03-15 18:26:54 +0800

    first code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'iqnr-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'iqnr-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iqnr-platform';
+    title = 'iqnr-platform';
 }
